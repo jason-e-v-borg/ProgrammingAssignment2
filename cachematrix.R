a0356abf209c3ffee488195747bdbc1fec4f7642
jason-e-v-borg <65714937+jason-e-v-borg@users.noreply.github.com>
2020-06-28 23:01:34 +1000

    Week 3, Programming Assessment 2 - Lexical Scoping
    This is the second Programming Assessment, which deals with Lexical Scoping (makeCacheMatrix and cacheSolve functions).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2: Caching the inverse of a matrix
 
-## Write a short comment describing this function
+## The makeCacheMatrix command creates a custom matrix.
+## The matrix is a list that holds a function that
+## defines and gets the value of the matrix
+## and sets and gets the inverse value of the matrixx
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  z <- NULL
+  set <- function(y) {
+    x <<- y
+    z <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) z <<- inverse
+  getinverse <- function() z
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function calculates the inverse of the matrix that was created with the makeCacheMatrix function.
+## cacheSolve first checks to see if the inverse has been calculated.
+## If it has, the inverse is gained from the cache, skipping the process
+## If not, it calculates the matrix's inverse and sets its value in the cache via setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return the inverse of a matrix, which is 'z'
+  z <- x$getinverse()
+  if(!is.null(z)) {
+    message("wait, compiling cached data")
+    return(z)
+  }
+  data <- x$get()
+  z <- solve(data, ...)
+  x$setinverse(z)
+  z
 }
